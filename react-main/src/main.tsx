044c4ac0a21432e92bc0446e60de48241b08e3fc
chiu.tszho <chiu.tszho@i3design.co.jp>
2023-07-04 13:22:04 +0900

    createFolder

--- react-main/src/main.tsx
+++ react-main/src/main.tsx
@@ -4,9 +4,9 @@ import App from './App';
 
 import './index.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import TaskList from './TaskList';
-import UserLogin from './UserLogin';
-import TaskCreate from './TaskCreate';
+import TaskCreate from './pages/TaskCreate/TaskCreate';
+import TaskList from './pages/TaskList/TaskList';
+import UserLogin from './pages/UserLogin/UserLogin';
 
 const router = createBrowserRouter([
   {

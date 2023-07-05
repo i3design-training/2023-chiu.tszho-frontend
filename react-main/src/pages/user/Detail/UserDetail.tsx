012f39b2896af249d97a1f939f1d49e7fb31d973
chiu.tszho <chiu.tszho@i3design.co.jp>
2023-07-05 10:57:46 +0900

    delete react-main内のリポジトリと不要なcode

--- react-main/src/pages/user/Detail/UserDetail.tsx
+++ react-main/src/pages/user/Detail/UserDetail.tsx
@@ -15,8 +15,6 @@ import styles from './UserDetail.module.css';
 
 function UserDetail() {
   const [showPassword, setShowPassword] = useState(false);
-  const [test, setTest] = useState(false);
-  const [test1, setTest1] = useState(false);
   const handleClickShowPassword = () => setShowPassword((show) => !show);
 
   const handleMouseDownPassword = (
